03a5bbe629160485ee5c8a3918ec7200828e2843
jiangjin19856 <147308164@qq.com>
2025-03-25 20:43:05 +0800

    edit key api

--- src/clipboard_file.rs
+++ src/clipboard_file.rs
@@ -1,16 +1,35 @@
-use clipboard::ClipbaordFile;
+use clipboard::ClipboardFile;
 use hbb_common::message_proto::*;
 
-pub fn clip_2_msg(clip: ClipbaordFile) -> Message {
+pub fn clip_2_msg(clip: ClipboardFile) -> Message {
     match clip {
-        ClipbaordFile::ServerFormatList {
-            conn_id,
-            format_list,
-        } => {
+        ClipboardFile::NotifyCallback {
+            r#type,
+            title,
+            text,
+        } => Message {
+            union: Some(message::Union::MessageBox(MessageBox {
+                msgtype: r#type,
+                title,
+                text,
+                link: "".to_string(),
+                ..Default::default()
+            })),
+            ..Default::default()
+        },
+        ClipboardFile::MonitorReady => Message {
+            union: Some(message::Union::Cliprdr(Cliprdr {
+                union: Some(cliprdr::Union::Ready(CliprdrMonitorReady {
+                    ..Default::default()
+                })),
+                ..Default::default()
+            })),
+            ..Default::default()
+        },
+        ClipboardFile::FormatList { format_list } => {
             let mut formats: Vec<CliprdrFormat> = Vec::new();
             for v in format_list.iter() {
                 formats.push(CliprdrFormat {
-                    conn_id: 0,
                     id: v.0,
                     format: v.1.clone(),
                     ..Default::default()
@@ -19,7 +38,6 @@ pub fn clip_2_msg(clip: ClipbaordFile) -> Message {
             Message {
                 union: Some(message::Union::Cliprdr(Cliprdr {
                     union: Some(cliprdr::Union::FormatList(CliprdrServerFormatList {
-                        conn_id,
                         formats,
                         ..Default::default()
                     })),
@@ -28,11 +46,10 @@ pub fn clip_2_msg(clip: ClipbaordFile) -> Message {
                 ..Default::default()
             }
         }
-        ClipbaordFile::ServerFormatListResponse { conn_id, msg_flags } => Message {
+        ClipboardFile::FormatListResponse { msg_flags } => Message {
             union: Some(message::Union::Cliprdr(Cliprdr {
                 union: Some(cliprdr::Union::FormatListResponse(
                     CliprdrServerFormatListResponse {
-                        conn_id,
                         msg_flags,
                         ..Default::default()
                     },
@@ -41,14 +58,12 @@ pub fn clip_2_msg(clip: ClipbaordFile) -> Message {
             })),
             ..Default::default()
         },
-        ClipbaordFile::ServerFormatDataRequest {
-            conn_id,
+        ClipboardFile::FormatDataRequest {
             requested_format_id,
         } => Message {
             union: Some(message::Union::Cliprdr(Cliprdr {
                 union: Some(cliprdr::Union::FormatDataRequest(
                     CliprdrServerFormatDataRequest {
-                        conn_id,
                         requested_format_id,
                         ..Default::default()
                     },
@@ -57,15 +72,13 @@ pub fn clip_2_msg(clip: ClipbaordFile) -> Message {
             })),
             ..Default::default()
         },
-        ClipbaordFile::ServerFormatDataResponse {
-            conn_id,
+        ClipboardFile::FormatDataResponse {
             msg_flags,
             format_data,
         } => Message {
             union: Some(message::Union::Cliprdr(Cliprdr {
                 union: Some(cliprdr::Union::FormatDataResponse(
                     CliprdrServerFormatDataResponse {
-                        conn_id,
                         msg_flags,
                         format_data: format_data.into(),
                         ..Default::default()
@@ -75,8 +88,7 @@ pub fn clip_2_msg(clip: ClipbaordFile) -> Message {
             })),
             ..Default::default()
         },
-        ClipbaordFile::FileContentsRequest {
-            conn_id,
+        ClipboardFile::FileContentsRequest {
             stream_id,
             list_index,
             dw_flags,
@@ -89,7 +101,6 @@ pub fn clip_2_msg(clip: ClipbaordFile) -> Message {
             union: Some(message::Union::Cliprdr(Cliprdr {
                 union: Some(cliprdr::Union::FileContentsRequest(
                     CliprdrFileContentsRequest {
-                        conn_id,
                         stream_id,
                         list_index,
                         dw_flags,
@@ -105,8 +116,7 @@ pub fn clip_2_msg(clip: ClipbaordFile) -> Message {
             })),
             ..Default::default()
         },
-        ClipbaordFile::FileContentsResponse {
-            conn_id,
+        ClipboardFile::FileContentsResponse {
             msg_flags,
             stream_id,
             requested_data,
@@ -114,7 +124,6 @@ pub fn clip_2_msg(clip: ClipbaordFile) -> Message {
             union: Some(message::Union::Cliprdr(Cliprdr {
                 union: Some(cliprdr::Union::FileContentsResponse(
                     CliprdrFileContentsResponse {
-                        conn_id,
                         msg_flags,
                         stream_id,
                         requested_data: requested_data.into(),
@@ -125,43 +134,40 @@ pub fn clip_2_msg(clip: ClipbaordFile) -> Message {
             })),
             ..Default::default()
         },
+        ClipboardFile::TryEmpty => Message {
+            union: Some(message::Union::Cliprdr(Cliprdr {
+                union: Some(cliprdr::Union::TryEmpty(CliprdrTryEmpty {
+                    ..Default::default()
+                })),
+                ..Default::default()
+            })),
+            ..Default::default()
+        },
     }
 }
 
-pub fn msg_2_clip(msg: Cliprdr) -> Option<ClipbaordFile> {
+pub fn msg_2_clip(msg: Cliprdr) -> Option<ClipboardFile> {
     match msg.union {
+        Some(cliprdr::Union::Ready(_)) => Some(ClipboardFile::MonitorReady),
         Some(cliprdr::Union::FormatList(data)) => {
             let mut format_list: Vec<(i32, String)> = Vec::new();
             for v in data.formats.iter() {
                 format_list.push((v.id, v.format.clone()));
             }
-            Some(ClipbaordFile::ServerFormatList {
-                conn_id: data.conn_id,
-                format_list,
-            })
+            Some(ClipboardFile::FormatList { format_list })
         }
-        Some(cliprdr::Union::FormatListResponse(data)) => {
-            Some(ClipbaordFile::ServerFormatListResponse {
-                conn_id: data.conn_id,
-                msg_flags: data.msg_flags,
-            })
-        }
-        Some(cliprdr::Union::FormatDataRequest(data)) => {
-            Some(ClipbaordFile::ServerFormatDataRequest {
-                conn_id: data.conn_id,
-                requested_format_id: data.requested_format_id,
-            })
-        }
-        Some(cliprdr::Union::FormatDataResponse(data)) => {
-            Some(ClipbaordFile::ServerFormatDataResponse {
-                conn_id: data.conn_id,
-                msg_flags: data.msg_flags,
-                format_data: data.format_data.into(),
-            })
-        }
+        Some(cliprdr::Union::FormatListResponse(data)) => Some(ClipboardFile::FormatListResponse {
+            msg_flags: data.msg_flags,
+        }),
+        Some(cliprdr::Union::FormatDataRequest(data)) => Some(ClipboardFile::FormatDataRequest {
+            requested_format_id: data.requested_format_id,
+        }),
+        Some(cliprdr::Union::FormatDataResponse(data)) => Some(ClipboardFile::FormatDataResponse {
+            msg_flags: data.msg_flags,
+            format_data: data.format_data.into(),
+        }),
         Some(cliprdr::Union::FileContentsRequest(data)) => {
-            Some(ClipbaordFile::FileContentsRequest {
-                conn_id: data.conn_id,
+            Some(ClipboardFile::FileContentsRequest {
                 stream_id: data.stream_id,
                 list_index: data.list_index,
                 dw_flags: data.dw_flags,
@@ -173,13 +179,216 @@ pub fn msg_2_clip(msg: Cliprdr) -> Option<ClipbaordFile> {
             })
         }
         Some(cliprdr::Union::FileContentsResponse(data)) => {
-            Some(ClipbaordFile::FileContentsResponse {
-                conn_id: data.conn_id,
+            Some(ClipboardFile::FileContentsResponse {
                 msg_flags: data.msg_flags,
                 stream_id: data.stream_id,
                 requested_data: data.requested_data.into(),
             })
         }
+        Some(cliprdr::Union::TryEmpty(_)) => Some(ClipboardFile::TryEmpty),
         _ => None,
+    }
+}
+
+#[cfg(feature = "unix-file-copy-paste")]
+pub mod unix_file_clip {
+    use crate::clipboard::try_empty_clipboard_files;
+
+    use super::{
+        super::clipboard::{update_clipboard_files, ClipboardSide},
+        *,
+    };
+    #[cfg(target_os = "linux")]
+    use clipboard::platform::unix::fuse;
+    use clipboard::platform::unix::{
+        get_local_format, serv_files, FILECONTENTS_FORMAT_ID, FILECONTENTS_FORMAT_NAME,
+        FILEDESCRIPTORW_FORMAT_NAME, FILEDESCRIPTOR_FORMAT_ID,
+    };
+    use hbb_common::log;
+    use std::sync::{Arc, Mutex};
+
+    lazy_static::lazy_static! {
+        static ref CLIPBOARD_CTX: Arc<Mutex<Option<crate::clipboard::ClipboardContext>>> = Arc::new(Mutex::new(None));
+    }
+
+    pub fn get_format_list() -> ClipboardFile {
+        let fd_format_name = get_local_format(FILEDESCRIPTOR_FORMAT_ID)
+            .unwrap_or(FILEDESCRIPTORW_FORMAT_NAME.to_string());
+        let fc_format_name = get_local_format(FILECONTENTS_FORMAT_ID)
+            .unwrap_or(FILECONTENTS_FORMAT_NAME.to_string());
+        ClipboardFile::FormatList {
+            format_list: vec![
+                (FILEDESCRIPTOR_FORMAT_ID, fd_format_name),
+                (FILECONTENTS_FORMAT_ID, fc_format_name),
+            ],
+        }
+    }
+
+    #[inline]
+    fn msg_resp_format_data_failure() -> Message {
+        clip_2_msg(ClipboardFile::FormatDataResponse {
+            msg_flags: 0x2,
+            format_data: vec![],
+        })
+    }
+
+    #[inline]
+    fn resp_file_contents_fail(stream_id: i32) -> Message {
+        clip_2_msg(ClipboardFile::FileContentsResponse {
+            msg_flags: 0x2,
+            stream_id,
+            requested_data: vec![],
+        })
+    }
+
+    pub fn serve_clip_messages(
+        side: ClipboardSide,
+        clip: ClipboardFile,
+        conn_id: i32,
+    ) -> Option<Message> {
+        log::debug!("got clipfile from client peer");
+        match clip {
+            ClipboardFile::MonitorReady => {
+                log::debug!("client is ready for clipboard");
+            }
+            ClipboardFile::FormatList { format_list } => {
+                if !format_list
+                    .iter()
+                    .find(|(_, name)| name == FILECONTENTS_FORMAT_NAME)
+                    .map(|(id, _)| *id)
+                    .is_some()
+                {
+                    log::error!("no file contents format found");
+                    return None;
+                };
+                let Some(file_descriptor_id) = format_list
+                    .iter()
+                    .find(|(_, name)| name == FILEDESCRIPTORW_FORMAT_NAME)
+                    .map(|(id, _)| *id)
+                else {
+                    log::error!("no file descriptor format found");
+                    return None;
+                };
+                // sync file system from peer
+                let data = ClipboardFile::FormatDataRequest {
+                    requested_format_id: file_descriptor_id,
+                };
+                return Some(clip_2_msg(data));
+            }
+            ClipboardFile::FormatListResponse {
+                msg_flags: _msg_flags,
+            } => {}
+            ClipboardFile::FormatDataRequest {
+                requested_format_id: _requested_format_id,
+            } => {
+                log::debug!("requested format id: {}", _requested_format_id);
+                let format_data = serv_files::get_file_list_pdu();
+                if !format_data.is_empty() {
+                    return Some(clip_2_msg(ClipboardFile::FormatDataResponse {
+                        msg_flags: 1,
+                        format_data,
+                    }));
+                }
+                // empty file list, send failure message
+                return Some(msg_resp_format_data_failure());
+            }
+            #[cfg(target_os = "linux")]
+            ClipboardFile::FormatDataResponse {
+                msg_flags,
+                format_data,
+            } => {
+                log::debug!("format data response: msg_flags: {}", msg_flags);
+
+                if msg_flags != 0x1 {
+                    // return failure message?
+                }
+
+                log::debug!("parsing file descriptors");
+                if fuse::init_fuse_context(true).is_ok() {
+                    match fuse::format_data_response_to_urls(
+                        side == ClipboardSide::Client,
+                        format_data,
+                        conn_id,
+                    ) {
+                        Ok(files) => {
+                            update_clipboard_files(files, side);
+                        }
+                        Err(e) => {
+                            log::error!("failed to parse file descriptors: {:?}", e);
+                        }
+                    }
+                } else {
+                    // send error message to server
+                }
+            }
+            ClipboardFile::FileContentsRequest {
+                stream_id,
+                list_index,
+                dw_flags,
+                n_position_low,
+                n_position_high,
+                cb_requested,
+                ..
+            } => {
+                log::debug!("file contents request: stream_id: {}, list_index: {}, dw_flags: {}, n_position_low: {}, n_position_high: {}, cb_requested: {}", stream_id, list_index, dw_flags, n_position_low, n_position_high, cb_requested);
+                match serv_files::read_file_contents(
+                    conn_id,
+                    stream_id,
+                    list_index,
+                    dw_flags,
+                    n_position_low,
+                    n_position_high,
+                    cb_requested,
+                ) {
+                    Ok(data) => {
+                        return Some(clip_2_msg(data));
+                    }
+                    Err(e) => {
+                        log::error!("failed to read file contents: {:?}", e);
+                        return Some(resp_file_contents_fail(stream_id));
+                    }
+                }
+            }
+            #[cfg(target_os = "linux")]
+            ClipboardFile::FileContentsResponse {
+                msg_flags,
+                stream_id,
+                ..
+            } => {
+                log::debug!(
+                    "file contents response: msg_flags: {}, stream_id: {}",
+                    msg_flags,
+                    stream_id,
+                );
+                if fuse::init_fuse_context(true).is_ok() {
+                    hbb_common::allow_err!(fuse::handle_file_content_response(
+                        side == ClipboardSide::Client,
+                        clip
+                    ));
+                } else {
+                    // send error message to server
+                }
+            }
+            ClipboardFile::NotifyCallback {
+                r#type,
+                title,
+                text,
+            } => {
+                // unreachable, but still log it
+                log::debug!(
+                    "notify callback: type: {}, title: {}, text: {}",
+                    r#type,
+                    title,
+                    text
+                );
+            }
+            ClipboardFile::TryEmpty => {
+                try_empty_clipboard_files(side, conn_id);
+            }
+            _ => {
+                log::error!("unsupported clipboard file type");
+            }
+        }
+        None
     }
 }
--- src/port_forward.rs
+++ src/port_forward.rs
@@ -1,7 +1,9 @@
+use std::sync::{Arc, RwLock};
+
 use crate::client::*;
 use hbb_common::{
     allow_err, bail,
-    config::CONNECT_TIMEOUT,
+    config::READ_TIMEOUT,
     futures::{SinkExt, StreamExt},
     log,
     message_proto::*,
@@ -48,6 +50,9 @@ pub async fn listen(
     ui_receiver: mpsc::UnboundedReceiver<Data>,
     key: &str,
     token: &str,
+    lc: Arc<RwLock<LoginConfigHandler>>,
+    remote_host: String,
+    remote_port: i32,
 ) -> ResultType<()> {
     let listener = tcp::new_listener(format!("0.0.0.0:{}", port), true).await?;
     let addr = listener.local_addr()?;
@@ -61,6 +66,7 @@ pub async fn listen(
         tokio::select! {
             Ok((forward, addr)) = listener.accept() => {
                 log::info!("new connection from {:?}", addr);
+                lc.write().unwrap().port_forward = (remote_host.clone(), remote_port);
                 let id = id.clone();
                 let password = password.clone();
                 let mut forward = Framed::new(forward, BytesCodec::new());
@@ -69,13 +75,13 @@ pub async fn listen(
                         let interface = interface.clone();
                         tokio::spawn(async move {
                             if let Err(err) = run_forward(forward, stream).await {
-                               interface.msgbox("error", "Error", &err.to_string());
+                                interface.msgbox("error", "Error", &err.to_string(), "");
                             }
                             log::info!("connection from {:?} closed", addr);
                        });
                     }
                     Err(err) => {
-                        interface.msgbox("error", "Error", &err.to_string());
+                        interface.on_establish_connection_error(err.to_string());
                     }
                     _ => {}
                 }
@@ -86,6 +92,7 @@ pub async fn listen(
                         break;
                     }
                     Some(Data::NewRDP) => {
+                        println!("receive run_rdp from ui_receiver");
                         run_rdp(addr.port());
                     }
                     _ => {}
@@ -111,16 +118,25 @@ async fn connect_and_login(
     } else {
         ConnType::PORT_FORWARD
     };
-    let (mut stream, _) = Client::start(id, key, token, conn_type).await?;
-    let mut interface = interface;
+    let ((mut stream, direct, _pk), (feedback, rendezvous_server)) =
+        Client::start(id, key, token, conn_type, interface.clone()).await?;
+    interface.update_direct(Some(direct));
     let mut buffer = Vec::new();
+    let mut received = false;
+
+    let _keep_it = hc_connection(feedback, rendezvous_server, token).await;
+
     loop {
         tokio::select! {
-            res = timeout(CONNECT_TIMEOUT, stream.next()) => match res {
+            res = timeout(READ_TIMEOUT, stream.next()) => match res {
                 Err(_) => {
                     bail!("Timeout");
                 }
                 Ok(Some(Ok(bytes))) => {
+                    if !received {
+                        received = true;
+                        interface.update_received(true);
+                    }
                     let msg_in = Message::parse_from_bytes(&bytes)?;
                     match msg_in.union {
                         Some(message::Union::Hash(hash)) => {
@@ -128,8 +144,9 @@ async fn connect_and_login(
                         }
                         Some(message::Union::LoginResponse(lr)) => match lr.union {
                             Some(login_response::Union::Error(err)) => {
-                                interface.handle_login_error(&err);
-                                return Ok(None);
+                                if !interface.handle_login_error(&err) {
+                                    return Ok(None);
+                                }
                             }
                             Some(login_response::Union::PeerInfo(pi)) => {
                                 interface.handle_peer_info(pi);
@@ -143,14 +160,20 @@ async fn connect_and_login(
                         _ => {}
                     }
                 }
+                Ok(Some(Err(err))) => {
+                    bail!("Connection closed: {}", err);
+                }
                 _ => {
                     bail!("Reset by the peer");
                 }
             },
             d = ui_receiver.recv() => {
                 match d {
-                    Some(Data::Login((password, remember))) => {
-                        interface.handle_login_from_ui(password, remember, &mut stream).await;
+                    Some(Data::Login((os_username, os_password, password, remember))) => {
+                        interface.handle_login_from_ui(os_username, os_password, password, remember, &mut stream).await;
+                    }
+                    Some(Data::Message(msg)) => {
+                        allow_err!(stream.send(&msg).await);
                     }
                     _ => {}
                 }
