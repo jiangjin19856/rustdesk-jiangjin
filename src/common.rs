01830cdf74f00af24d0b7e3a6580bd351244a6a3
jiangjin19856 <147308164@qq.com>
2025-04-01 09:51:27 +0800

    内置api服务

--- src/common.rs
+++ src/common.rs
@@ -930,7 +930,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://jiangjinyun.top:21114".to_owned()
+    "http://jiangjinyun.top".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
